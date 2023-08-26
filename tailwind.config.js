bcc3802341cb152d67c857a5579fb8be31f794e1
Natàlia Martínez <nsanchez.martinez@atsistemas.com>
2023-08-26 22:29:39 +0200

    home

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  content: [
-    "./_drafts/**/*.html",
-    "./_fitxes/**/*.html",
-    "./_includes/**/*.html",
-    "./_layouts/**/*.html",
-    "./_posts/*.md",
-    "./*.md",
-    "./*.html",
-  ],
-  theme: {
+    content: [
+        './_drafts/**/*.html',
+        './_fitxes/**/*.html',
+        './_includes/**/*.html',
+        './_layouts/**/*.html',
+        './_posts/*.md',
+        './*.md',
+        './*.html',
+    ],
     theme: {
-      extend: {},
+        fontFamily: {
+            serif: ['Lora', 'Georgia'],
+        },
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+}
